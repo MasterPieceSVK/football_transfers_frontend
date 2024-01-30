702e808ae8a3595f3e43a87c4721d3299a898000
MasterPieceSVK <112313437+MasterPieceSVK@users.noreply.github.com>
2024-01-30 19:56:37 +0100

    less results

--- src/components/MoreBtn.js
+++ src/components/MoreBtn.js
@@ -1,7 +1,7 @@
 export default function MoreBtn({ loadContent }) {
   return (
     <div className="flex justify-center my-4">
-      <button className="btn btn-primary" onClick={() => loadContent(100)}>
+      <button className="btn btn-primary" onClick={() => loadContent(40)}>
         More
       </button>
     </div>
